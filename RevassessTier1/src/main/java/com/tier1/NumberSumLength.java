dfddacd36c8b030f69987804abfd849b7187e7cb
James Featherstone <james.s.featherstone@gmail.com>
2020-06-01 15:00:13 -0400

    Finished Tier 1

--- RevassessTier1/src/main/java/com/tier1/NumberSumLength.java
+++ RevassessTier1/src/main/java/com/tier1/NumberSumLength.java
@@ -3,6 +3,12 @@ package com.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		int sumPowers = 0;
+		char[] digits = String.valueOf(num).toCharArray();
+		int len = digits.length;
+		for(char c: digits) {
+			sumPowers += Math.pow(Double.parseDouble(String.valueOf(c)), len);
+		}		
+		return num == sumPowers;
 	}
 }
--- RevassessTier1/src/main/java/com/tier1/SumOverArray.java
+++ RevassessTier1/src/main/java/com/tier1/SumOverArray.java
@@ -3,6 +3,14 @@ package com.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int answer = 0;
+		if(arr == null) {
+			return 0;
+		}
+		for(int i: arr) {
+			answer += i;
+			System.out.println(i);
+		}
+		return Math.abs(answer);
 	}
 }
